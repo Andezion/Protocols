2f68e7f9c0a02b450ee4385b7eb594820af74198
Andezion <245122@edu.p.lodz.pl>
2026-02-12 00:06:24 +0100

    add: started implemention

--- TransportLayer/TCP/server.c
+++ TransportLayer/TCP/server.c
@@ -1,5 +1,29 @@
 #include <stdio.h>
 
+typedef unsigned char byte;
+
+struct tcp_protocol {
+    byte source_port[16];
+    byte destination_port[16];
+
+    byte sequence_number[32];
+    byte acknowledgment_number[32];
+
+    byte data_offset[4];
+    byte reserved[3];
+
+    byte flags[9];
+    byte window_size[16];
+
+    byte checksum[16];
+    byte urgent_pointer[16];
+};
+
+int send_tcp_packet(struct tcp_protocol *packet) {
+    // Code to send TCP packet
+    return 0;
+}
+
 int main() {
     printf("Starting TCP server!\n");
     return 0;

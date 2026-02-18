00d6a82b3f1d779ce7dda906a080bdd841b6adf6
Andezion <245122@edu.p.lodz.pl>
2026-02-18 01:31:27 +0100

    added new stuff

--- TransportLayer/TCP/client.c
+++ TransportLayer/TCP/client.c
@@ -18,10 +18,11 @@ int main(void) {
 	serv.sin_family = AF_INET; // мы используем IPv4
 	serv.sin_port = htons(server_port);
 
-	inet_pton(AF_INET, server_ip, &serv.sin_addr);
+	inet_pton(AF_INET, server_ip, &serv.sin_addr); // преобразуем IP адрес сервера из текстового вида в бинарный и сохраняем его в структуре serv
 
-	uint32_t isn;
+	uint32_t isn; // храним ISN, который мы получаем от сервера в процессе рукопожатия, если нам нужно его вернуть, то мы передаем указатель на эту переменную в функцию tcp_connect, иначе мы передаем NULL
 	if (tcp_connect(s, (struct sockaddr *) &serv, sizeof(serv), &isn) == 0) {
+		// пробуем сделать рукопожатие с сервером, если оно прошло успешно, то мы получаем ISN сервера в переменную isn, иначе мы возвращаем -1
 		printf("Connected to %s:%u (ISN = %u)\n", server_ip, server_port, isn);
 	} else {
 		printf("Connection failed or timed out.\n");

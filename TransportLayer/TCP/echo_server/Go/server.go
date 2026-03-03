fd821d45d7b7034eaf80f5697a837090e09e70b3
Andezion <245122@edu.p.lodz.pl>
2026-03-04 00:59:48 +0100

    add: added echo Go stuff

--- TransportLayer/TCP/echo_server/Go/server.go
+++ TransportLayer/TCP/echo_server/Go/server.go
@@ -1,5 +1,32 @@
 package main
 
+import (
+	"container/list"
+	"fmt"
+	"log"
+	"net"
+)
+
 func main() {
+	listener, err := net.Listen("tcp", ":8090")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Echo server is listening on port 8090...")
+
+	clients := list.New()
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Println("Failed to accept connection:", err)
+			continue
+		}
+		fmt.Println("New client connected:", conn.RemoteAddr())
+		clients.PushBack(conn)
+
+		go handleClient(conn, clients)
+	}
 
 }

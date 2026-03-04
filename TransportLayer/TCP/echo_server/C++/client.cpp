e49b8a6cce8e56b617313d4ff3f296c924bcb5c0
Andezion <245122@edu.p.lodz.pl>
2026-03-04 17:52:36 +0100

    add: started cpp impl

--- TransportLayer/TCP/echo_server/C++/client.cpp
+++ TransportLayer/TCP/echo_server/C++/client.cpp
@@ -1,6 +1,7 @@
-#include <boost/asio/io_context.hpp>
+#include <boost/asio/streambuf.hpp>
 #include <string>
 #include <boost/asio.hpp>
+#include <iostream>
 
 using boost::asio::ip::tcp;
 
@@ -8,7 +9,18 @@ int main() {
     boost::asio::io_context io_context;
     tcp::resolver resolver(io_context);
 
+    auto endpoints = resolver.resolve("127.0.0.1", "8090");
+    tcp::socket socket(io_context);
 
-    
+    boost::asio::connect(socket, endpoints);
+    std::string message = "Hello from the client!\n";
+
+    boost::asio::write(socket, boost::asio::buffer(message));
+
+    boost::asio::streambuf response;
+    boost::asio::read_until(socket, response, '\n');
+
+    std::cout << std::string{buffers_begin(response.data()), buffers_end(response.data())};
+
     return 0;
 }

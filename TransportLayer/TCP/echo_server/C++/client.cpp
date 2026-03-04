614df197b6a3d89387840afe47607e338f64b74e
Andezion <245122@edu.p.lodz.pl>
2026-03-04 17:49:11 +0100

    add: started cpp impl

--- TransportLayer/TCP/echo_server/C++/client.cpp
+++ TransportLayer/TCP/echo_server/C++/client.cpp
@@ -1,9 +1,14 @@
+#include <boost/asio/io_context.hpp>
 #include <string>
 #include <boost/asio.hpp>
 
 using boost::asio::ip::tcp;
 
 int main() {
+    boost::asio::io_context io_context;
+    tcp::resolver resolver(io_context);
+
+
     
     return 0;
 }

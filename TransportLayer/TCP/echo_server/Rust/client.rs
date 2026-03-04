96b093930d863aa47880badfaaa1bcf8ee318d81
Andezion <245122@edu.p.lodz.pl>
2026-03-04 13:59:42 +0100

    fix: fixing rust implementation

--- TransportLayer/TCP/echo_server/Rust/client.rs
+++ TransportLayer/TCP/echo_server/Rust/client.rs
@@ -1,5 +1,8 @@
 use std::{io, net::{TcpListener, TcpStream}, thread};
 
 fn main() -> io::Result<()> {
-    
+    let addr = "127.0.0.1:8090";
+    let stream = TcpStream::connect(addr)?;
+    println!("Connected to server at {}", addr);
+    Ok(())
 }

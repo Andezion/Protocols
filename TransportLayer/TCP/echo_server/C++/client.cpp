77fc1789f655a587b0d4c72b12c37e6e1927465d
Andezion <245122@edu.p.lodz.pl>
2026-03-04 17:32:41 +0100

    add: started cpp impl

--- TransportLayer/TCP/echo_server/C++/client.cpp
+++ TransportLayer/TCP/echo_server/C++/client.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
+#include <array>
 #include <boost/asio.hpp>
 
 using boost::asio::ip::tcp;
 
-int main() {
-    
+int main(int argc, char* argv[]) {
+    try {
+        if (argc != 2) {
+            std::err << "Usage: client <host>" << std::endl;
+            return 1;
+        }
+    }
+
+    boost::asio::io_context io_context;
+    tcp::resolver resolver(io_context);
+
     return 0;
 }

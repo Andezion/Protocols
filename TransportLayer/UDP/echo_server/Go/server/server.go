c6a885c01e86a245c5a063f4fdc6801e2920b15d
Andezion <245122@edu.p.lodz.pl>
2026-03-11 13:31:43 +0100

    add: started udp

--- TransportLayer/UDP/echo_server/Go/server/server.go
+++ TransportLayer/UDP/echo_server/Go/server/server.go
@@ -14,4 +14,27 @@ func main() {
 
 	fmt.Println("Echo server is listening on port 8090...")
 
+	conn, err := listener.ReadFrom()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Client connected: %s\n", conn.String())
+
+	buf := make([]byte, 1024)
+	n, addr, err := listener.ReadFrom(buf)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Received from client: %s\n", string(buf[:n]))
+
+	_, err = listener.WriteTo(buf[:n], addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Echoed back to client.")
+
+	conn.Close()
 }

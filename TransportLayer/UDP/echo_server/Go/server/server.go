f0386c67e0242a479f5c1b073576d3d7cb810fca
Andezion <245122@edu.p.lodz.pl>
2026-03-11 13:28:43 +0100

    add: started udp

--- TransportLayer/UDP/echo_server/Go/server/server.go
+++ TransportLayer/UDP/echo_server/Go/server/server.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net"
+)
+
 func main() {
+	listener, err := net.ListenPacket("udp", ":8090")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Echo server is listening on port 8090...")
 
 }

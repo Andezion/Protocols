ddfbc1eba22c5c122a7d2b57fe7d6f318393b0e6
Andezion <245122@edu.p.lodz.pl>
2026-03-11 13:54:20 +0100

    add: started rust

--- TransportLayer/UDP/echo_server/Rust/client.rs
+++ TransportLayer/UDP/echo_server/Rust/client.rs
@@ -5,4 +5,13 @@ fn main() -> io::Result<()> {
     let addr = "127:0.0.1:8090";
     let socket = UdpSocket::bind(addr)?;
     println!("Connected to server at {}", addr);
+
+    let msg = b"hello from rust client\n";
+    socket.send_to(msg, addr)?;
+
+    let mut buf = [0u8; 1024];
+    let n = socket.recv(&mut buf)?;
+    println!("Received echo: {}", String::from_utf8_lossy(&buf[..n]));
+
+    Ok(())
 }

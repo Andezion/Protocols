a8727be19d33ff52d6201706ddf4f023cf5663ac
Andezion <245122@edu.p.lodz.pl>
2026-03-11 13:53:31 +0100

    add: started rust

--- TransportLayer/UDP/echo_server/Rust/client.rs
+++ TransportLayer/UDP/echo_server/Rust/client.rs
@@ -2,5 +2,7 @@ use std::io::{self, Read, Write};
 use std::net::UdpSocket;
 
 fn main() -> io::Result<()> {
-    
+    let addr = "127:0.0.1:8090";
+    let socket = UdpSocket::bind(addr)?;
+    println!("Connected to server at {}", addr);
 }

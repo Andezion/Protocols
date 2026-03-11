e99df2f5620f9ebc9f2f77d78dee6f6a2bbbe662
Andezion <245122@edu.p.lodz.pl>
2026-03-11 13:51:36 +0100

    add: started rust

--- TransportLayer/UDP/echo_server/Rust/server.rs
+++ TransportLayer/UDP/echo_server/Rust/server.rs
@@ -2,5 +2,6 @@ use std::io::{self, Read, Write};
 use std::net::UdpListener;
 
 fn main() -> io::Result<()> {
-    
+    let listener = UdpListener::bind("0.0.0.0:8090")?;S
+    println!("One-shot echo server listening on 0.0.0.0:8090");
 }

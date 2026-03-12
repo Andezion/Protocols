40218064bdd51a8cec300d67861ab7d7c7b996cf
Andezion <245122@edu.p.lodz.pl>
2026-03-12 14:31:26 +0100

    add: added comments

--- TransportLayer/UDP/echo_server/C++/server.cpp
+++ TransportLayer/UDP/echo_server/C++/server.cpp
@@ -6,14 +6,15 @@
 using boost::asio::ip::udp;
 
 int main() {
+    // Занимаем порт 8090 для прослушивания входящих сообщений
     boost::asio::io_context io_context;
     udp::socket socket(io_context, udp::endpoint(udp::v4(), 8090));
 
     std::cout << "UDP Echo Server is running on port 8090..." << std::endl;
 
     char data[1024];
-    udp::endpoint sender_endpoint;
-    boost::system::error_code ec;
+    udp::endpoint sender_endpoint; // Создаём место куда кидаем информацию о клиенте, который нам что-то отправил
+    boost::system::error_code ec; // Создаём объект для хранения ошибок, которые могут возникнуть при приёме или отправке данных
 
     size_t length = socket.receive_from(boost::asio::buffer(data), sender_endpoint, 0, ec);
     if (ec) {

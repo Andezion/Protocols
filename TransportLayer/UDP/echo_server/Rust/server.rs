1bc6cf8db8012cf7cd9b1f7f6256fa69b758326f
Andezion <245122@edu.p.lodz.pl>
2026-03-12 14:35:35 +0100

    add: added comments

--- TransportLayer/UDP/echo_server/Rust/server.rs
+++ TransportLayer/UDP/echo_server/Rust/server.rs
@@ -2,16 +2,17 @@ use std::io;
 use std::net::UdpSocket;
 
 fn main() -> io::Result<()> {
+    // Биндим сокет на все интерфейсы и порт 8090
     let socket = UdpSocket::bind("0.0.0.0:8090")?;
     println!("One-shot UDP echo server listening on 0.0.0.0:8090");
 
     let mut buf = [0u8; 1024];
-    let (n, src) = socket.recv_from(&mut buf)?;
+    let (n, src) = socket.recv_from(&mut buf)?; // Получаем данные и адрес отправителя
     
     println!("Received {} bytes from {}", n, src);
     println!("Received: {}", String::from_utf8_lossy(&buf[..n]));
 
-    socket.send_to(&buf[..n], src)?;
+    socket.send_to(&buf[..n], src)?; // Отправляем обратно полученные данные
     println!("Echoed back and exiting.");
 
     Ok(())

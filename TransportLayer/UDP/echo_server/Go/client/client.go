a5f344b6ef73a9ee83798632ba7e7b3ddf60acdf
Andezion <245122@edu.p.lodz.pl>
2026-03-12 14:34:53 +0100

    add: added comments

--- TransportLayer/UDP/echo_server/Go/client/client.go
+++ TransportLayer/UDP/echo_server/Go/client/client.go
@@ -8,24 +8,26 @@ import (
 )
 
 func main() {
+	// Адрес сервера, к которому будем подключаться
 	addr := "127.0.0.1:8090"
-	conn, err := net.Dial("udp", addr)
+	conn, err := net.Dial("udp", addr) // Устанавливаем UDP соединение с сервером
 	if err != nil {
 		log.Fatalf("Failed to connect to %s: %v", addr, err)
 	}
 
+	// Закрываем соединение при завершении функции main
 	defer conn.Close()
 
 	fmt.Printf("Connected to server %s\n", addr)
 
 	message := "Hello from client\n"
-	_, err = conn.Write([]byte(message))
+	_, err = conn.Write([]byte(message)) // Отправляем сообщение серверу
 	if err != nil {
 		log.Fatalf("Failed to send message: %v", err)
 	}
 
 	buf := make([]byte, 1024)
-	n, err := conn.Read(buf)
+	n, err := conn.Read(buf) // Читаем ответ от сервера
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Error reading response:", err)
 		return

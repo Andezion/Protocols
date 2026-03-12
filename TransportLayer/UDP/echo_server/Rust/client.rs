8f90a21b2320faa214b7efd1e1fa9eb16d451f28
Andezion <245122@edu.p.lodz.pl>
2026-03-12 14:38:04 +0100

    add: added comments

--- TransportLayer/UDP/echo_server/Rust/client.rs
+++ TransportLayer/UDP/echo_server/Rust/client.rs
@@ -2,15 +2,16 @@ use std::io::{self};
 use std::net::UdpSocket;
 
 fn main() -> io::Result<()> {
+    // Биндим сокет на локальный адрес и порт 0 (выбирается автоматически)
     let addr = "127.0.0.1:8090";
     let socket = UdpSocket::bind(addr)?;
     println!("Connected to server at {}", addr);
 
     let msg = b"hello from rust client\n";
-    socket.send_to(msg, addr)?;
+    socket.send_to(msg, addr)?; // Отправляем сообщение на сервер
 
     let mut buf = [0u8; 1024];
-    let n = socket.recv(&mut buf)?;
+    let n = socket.recv(&mut buf)?; // Получаем ответ от сервера
     println!("Received echo: {}", String::from_utf8_lossy(&buf[..n]));
 
     Ok(())

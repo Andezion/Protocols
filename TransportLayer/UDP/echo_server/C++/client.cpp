33e6e8c537f766e7056837515a096bb1911d5585
Andezion <245122@edu.p.lodz.pl>
2026-03-12 13:20:45 +0100

    add: started c++ udp

--- TransportLayer/UDP/echo_server/C++/client.cpp
+++ TransportLayer/UDP/echo_server/C++/client.cpp
@@ -5,28 +5,22 @@
 using boost::asio::ip::udp;
 
 int main() {
-	try {
-		boost::asio::io_context io_context;
+	boost::asio::io_context io_context;
 
-		udp::resolver resolver(io_context);
-		auto endpoints = resolver.resolve(udp::v4(), "127.0.0.1", "8090");
+    udp::resolver resolver(io_context);
+    auto endpoints = resolver.resolve(udp::v4(), "127.0.0.1", "8090");
 
-		udp::socket socket(io_context);
-		socket.open(udp::v4());
+    udp::socket socket(io_context);
+    socket.open(udp::v4());
 
-		std::string msg = "Hello from client";
-		socket.send_to(boost::asio::buffer(msg), *endpoints.begin());
+    std::string msg = "Hello from client";
+    socket.send_to(boost::asio::buffer(msg), *endpoints.begin());
 
-		char reply[1024];
-		udp::endpoint sender;
-		size_t len = socket.receive_from(boost::asio::buffer(reply), sender);
-		std::string received(reply, len);
-		std::cout << "Server reply: " << received << std::endl;
-
-	} catch (std::exception &e) {
-		std::cerr << "Client exception: " << e.what() << std::endl;
-		return 1;
-	}
+    char reply[1024];
+    udp::endpoint sender;
+    size_t len = socket.receive_from(boost::asio::buffer(reply), sender);
+    std::string received(reply, len);
+    std::cout << "Server reply: " << received << std::endl;
 
 	return 0;
 }

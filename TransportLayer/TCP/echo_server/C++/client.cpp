ea493bb6b657d7c3ffb8d2fd86a0e9801040e221
Andezion <245122@edu.p.lodz.pl>
2026-03-12 14:23:02 +0100

    add: added comments

--- TransportLayer/TCP/echo_server/C++/client.cpp
+++ TransportLayer/TCP/echo_server/C++/client.cpp
@@ -6,12 +6,15 @@
 using boost::asio::ip::tcp;
 
 int main() {
+    // Создаем io_context и resolver для разрешения адреса сервера
     boost::asio::io_context io_context;
     tcp::resolver resolver(io_context);
 
+    // Разрешаем адрес сервера и порт
     auto endpoints = resolver.resolve("127.0.0.1", "8090");
     tcp::socket socket(io_context);
 
+    // Подключаемся к серверу
     boost::asio::connect(socket, endpoints);
     std::string message = "Hello from the client!\n";
 

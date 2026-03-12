e0306af2f44bdadcbf42843af5dc5d5387b2d131
Andezion <245122@edu.p.lodz.pl>
2026-03-12 14:32:19 +0100

    add: added comments

--- TransportLayer/UDP/echo_server/C++/server.cpp
+++ TransportLayer/UDP/echo_server/C++/server.cpp
@@ -16,15 +16,18 @@ int main() {
     udp::endpoint sender_endpoint; // Создаём место куда кидаем информацию о клиенте, который нам что-то отправил
     boost::system::error_code ec; // Создаём объект для хранения ошибок, которые могут возникнуть при приёме или отправке данных
 
+    // Получаем данные от клиента
     size_t length = socket.receive_from(boost::asio::buffer(data), sender_endpoint, 0, ec);
     if (ec) {
         std::cerr << "Receive error: " << ec.message() << std::endl;
         return 1;
     }
 
+    // Выводим информацию о клиенте и полученное сообщение
     std::string received(data, length);
     std::cout << "Received from client: " << sender_endpoint.address().to_string() << ":" << sender_endpoint.port() << " " << received << std::endl;
 
+    // Отправляем обратно клиенту полученное сообщение
     std::string message = "Hello from server!";
     socket.send_to(boost::asio::buffer(message), sender_endpoint, 0, ec);
     if (ec) {

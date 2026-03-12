8caa8ccf7a10b6794a6443e93151a5f26f9287c0
Andezion <245122@edu.p.lodz.pl>
2026-03-12 14:28:52 +0100

    add: added comments

--- TransportLayer/TCP/echo_server/Rust/server.rs
+++ TransportLayer/TCP/echo_server/Rust/server.rs
@@ -5,7 +5,6 @@ fn main() -> io::Result<()> {
     let listener = TcpListener::bind("0.0.0.0:8090")?;
     println!("One-shot echo server listening on 0.0.0.0:8090");
 
-    // Accept exactly one connection, read once, echo once, exit.
     let (mut stream, addr) = listener.accept()?;
     println!("Client connected: {}", addr);
 

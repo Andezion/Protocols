52d9d99ad058469d96b1e5a3f7491d6f0fbe2ec4
Andezion <245122@edu.p.lodz.pl>
2026-03-12 13:13:15 +0100

    add: started c++ udp

--- TransportLayer/UDP/echo_server/C++/server.cpp
+++ TransportLayer/UDP/echo_server/C++/server.cpp
@@ -1,3 +1,4 @@
+#include <boost/asio/ip/udp.hpp>
 #include <string>
 #include <boost/asio.hpp>
 #include <iostream>
@@ -6,5 +7,10 @@ using boost::asio::ip::udp;
 
 int main() {
     boost::asio::io_context io_context;
+    udp::socket socket(io_context, udp::endpoint(udp::v4(), 8090)); 
+
+    std::cout << "UDP Echo Server is running on port 8090..." << std::endl;
+
     
+
 }

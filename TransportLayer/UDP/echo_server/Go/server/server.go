ad552c5f0e25828eec6a13666e85e7006c55a2ba
Andezion <245122@edu.p.lodz.pl>
2026-03-12 14:34:23 +0100

    add: added comments

--- TransportLayer/UDP/echo_server/Go/server/server.go
+++ TransportLayer/UDP/echo_server/Go/server/server.go
@@ -7,16 +7,20 @@ import (
 )
 
 func main() {
+	// Говорим, что создали UDP сервер, который слушает порт 8090
 	pc, err := net.ListenPacket("udp", ":8090")
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	// Закрываем соединение при завершении функции main
 	defer pc.Close()
 
 	fmt.Println("Echo server is listening on port 8090...")
 
-	buf := make([]byte, 2048)
+	buf := make([]byte, 2048) // Буфер для чтения данных от клиента
 
+	// Принимаем данные от клиента и отправляем их обратно
 	n, addr, err := pc.ReadFrom(buf)
 	if err != nil {
 		log.Println("read error:", err)
@@ -24,6 +28,7 @@ func main() {
 
 	fmt.Printf("Received %d bytes from %s: %s\n", n, addr.String(), string(buf[:n]))
 
+	// Отправляем полученные данные обратно клиенту
 	_, err = pc.WriteTo(buf[:n], addr)
 	if err != nil {
 		log.Println("write error:", err)

d138de52e56aa0d80996ef85c071078e0b5ca726
Andezion <245122@edu.p.lodz.pl>
2026-03-19 00:33:24 +0100

    add: finished rust server

--- TransportLayer/TCP/multiple_clients/Rust/server.rs
+++ TransportLayer/TCP/multiple_clients/Rust/server.rs
@@ -2,16 +2,18 @@ use std::io::{self, Read, Write};
 use std::net::{TcpListener, TcpStream};
 
 fn main() -> io::Result<()> {
+    // занимаем место 
     let listener = TcpListener::bind("0.0.0.0:8090")?;
     println!("Multi-client echo server listening on 0.0.0.0:8090");
     
-    loop {
+    loop { // и тут снова разница - у нас просто бесконечный цикл
         let (mut stream, addr) = listener.accept()?;
         println!("Client connected: {}", addr);
         
         // Обрабатываем клиента в отдельном потоке
-        std::thread::spawn(move || {
+        std::thread::spawn(move || {    
             let mut buf = [0u8; 1024];
+            // Читаем данные от клиента в цикле, пока он не отключится
             loop {
                 let n = match stream.read(&mut buf) {
                     Ok(0) => {

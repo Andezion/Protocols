60f02398ec44cc4de03ca82044f137f0bf783f21
Andezion <245122@edu.p.lodz.pl>
2026-03-19 00:12:23 +0100

    add: started multiple clients server tcp in go

--- TransportLayer/TCP/multiple_clients/Go/client/client.go
+++ TransportLayer/TCP/multiple_clients/Go/client/client.go
@@ -1,1 +1,39 @@
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+	addr := "127.0.0.1:8090"
+	conn, err := net.Dial("tcp", addr)
+	if err != nil {
+		log.Fatalf("Failed to connect to %s: %v", addr, err)
+	}
+
+	defer conn.Close()
+
+	fmt.Printf("Connected to server %s\n", addr)
+
+	message := "Hello from client\n"
+	_, err = conn.Write([]byte(message))
+	if err != nil {
+		log.Fatalf("Failed to send message: %v", err)
+	}
+
+	reader := bufio.NewReader(conn)
+	resp, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error reading response:", err)
+		return
+	}
+
+	fmt.Printf("Received from server: %s", resp)
+
+	fmt.Println("Client finished one handshake and will exit.")
+
+}

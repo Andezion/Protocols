7d9d298834c8db01be19b82b3188bf0ab8a3fe25
Andezion <245122@edu.p.lodz.pl>
2026-03-18 22:59:46 +0100

    add: started multiple clients server tcp in go

--- TransportLayer/TCP/multiple_clients/Go/server/server.go
+++ TransportLayer/TCP/multiple_clients/Go/server/server.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+)
+
+func main() {
+	listner, err := net.Listen("tcp", ":8090")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Echo server is listening on port 8090...")
+}

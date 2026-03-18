7000837211935c935b16d2cc96eb720a441cb097
Andezion <245122@edu.p.lodz.pl>
2026-03-19 00:31:39 +0100

    add: finished rust server

--- TransportLayer/TCP/multiple_clients/Rust/server.rs
+++ TransportLayer/TCP/multiple_clients/Rust/server.rs
@@ -4,5 +4,36 @@ use std::net::{TcpListener, TcpStream};
 fn main() -> io::Result<()> {
     let listener = TcpListener::bind("0.0.0.0:8090")?;
     println!("Multi-client echo server listening on 0.0.0.0:8090");
+    
+    loop {
+        let (mut stream, addr) = listener.accept()?;
+        println!("Client connected: {}", addr);
+        
+        // Обрабатываем клиента в отдельном потоке
+        std::thread::spawn(move || {
+            let mut buf = [0u8; 1024];
+            loop {
+                let n = match stream.read(&mut buf) {
+                    Ok(0) => {
+                        println!("Client {} disconnected", addr);
+                        break;
+                    },
+                    Ok(n) => n,
+                    Err(e) => {
+                        eprintln!("Error reading from client {}: {}", addr, e);
+                        break;
+                    }
+                };
+                
+                println!("Received from {}: {}", addr, String::from_utf8_lossy(&buf[..n]));
+                
+                if let Err(e) = stream.write_all(&buf[..n]) {
+                    eprintln!("Error writing to client {}: {}", addr, e);
+                    break;
+                }
+            }
+        });
+    }
+    
     Ok(())
 }

a8f635af3c84d37fc8f151bc5e5d6d205bf76929
Andezion <245122@edu.p.lodz.pl>
2026-03-19 00:17:40 +0100

    add: added desc

--- TransportLayer/TCP/multiple_clients/Go/client/client.go
+++ TransportLayer/TCP/multiple_clients/Go/client/client.go
@@ -8,23 +8,27 @@ import (
 	"os"
 )
 
+// как и обычный клиент
 func main() {
+	// подключаемся к серверу, который должен быть запущен на этом же адресе и порту
 	addr := "127.0.0.1:8090"
 	conn, err := net.Dial("tcp", addr)
 	if err != nil {
 		log.Fatalf("Failed to connect to %s: %v", addr, err)
 	}
 
-	defer conn.Close()
+	defer conn.Close() // гарантируем, что соединение будет закрыто после завершения работы клиента
 
 	fmt.Printf("Connected to server %s\n", addr)
 
+	// клиент кричит
 	message := "Hello from client\n"
 	_, err = conn.Write([]byte(message))
 	if err != nil {
 		log.Fatalf("Failed to send message: %v", err)
 	}
 
+	// слушаем что кричат в ответ
 	reader := bufio.NewReader(conn)
 	resp, err := reader.ReadString('\n')
 	if err != nil {

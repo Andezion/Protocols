0673af35b37646ace949e5da2a59271fb7bb6289
Andezion <245122@edu.p.lodz.pl>
2026-03-19 00:29:01 +0100

    add: started doing rust tcp multi server

--- TransportLayer/TCP/multiple_clients/Rust/server.rs
+++ TransportLayer/TCP/multiple_clients/Rust/server.rs
@@ -2,5 +2,7 @@ use std::io::{self, Read, Write};
 use std::net::{TcpListener, TcpStream};
 
 fn main() -> io::Result<()> {
-    
+    let listener = TcpListener::bind("0.0.0.0:8090")?;
+    println!("Multi-client echo server listening on 0.0.0.0:8090");
+    Ok(())
 }

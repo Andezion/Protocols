3459bb2c53e1f328bbcd3d4b0834235a63c4f943
Andezion <245122@edu.p.lodz.pl>
2026-03-19 01:01:16 +0100

    add: added new c++ multi

--- TransportLayer/TCP/multiple_clients/C++/server.cpp
+++ TransportLayer/TCP/multiple_clients/C++/server.cpp
@@ -5,5 +5,25 @@
 using boost::asio::ip::tcp;
 
 int main() {
-    
+    boost::asio::io_context io_context;
+    tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(),8090));
+
+    std::cout << "Server is running on port 8090..." << std::endl;
+
+    while (true) {
+        tcp::socket socket(io_context);
+        acceptor.accept(socket);
+
+        std::string message = "Hello from the server!";
+
+        boost::asio::streambuf buffer;
+        boost::asio::read_until(socket, buffer, '\n');
+
+        std::string client_msg{buffers_begin(buffer.data()), buffers_end(buffer.data())};
+        std::cout << "Received from client: " << client_msg << std::endl;
+
+        boost::asio::write(socket, boost::asio::buffer(message + "\n"));
+    }
+
+    return 0;   
 }

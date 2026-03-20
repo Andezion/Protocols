78b4e554c98fdecd8d5a818a898ca3e8f00d575e
Andezion <245122@edu.p.lodz.pl>
2026-03-20 17:44:50 +0100

    add: started graceful shitdown rust

--- TransportLayer/TCP/graceful_disconnect/Rust/server.rs
+++ TransportLayer/TCP/graceful_disconnect/Rust/server.rs
@@ -1,2 +1,39 @@
 use std::io::{self, Read, Write};
 use std::net::{TcpListener, TcpStream};
+
+fn main() -> io::Result<()> {
+    let listener = TcpListener::bind("0.0.0.0:8090")?;
+    println!("Echo server listening on 0.0.0.0:8090");
+    
+    loop {
+        let (mut stream, addr) = listener.accept()?;
+        println!("Client connected: {}", addr);
+        
+        std::thread::spawn(move || {
+            let mut buf = [0u8; 1024];
+            loop {
+                let n = match stream.read(&mut buf) {
+                    Ok(0) => {
+                        println!("Client {} disconnected", addr);
+                        
+                        break;
+                    },
+                    Ok(n) => n,
+                    Err(e) => {
+                        eprintln!("Error reading from client {}: {}", addr, e);
+                        break;
+                    }
+                };
+                
+                println!("Received from {}: {}", addr, String::from_utf8_lossy(&buf[..n]));
+                
+                if let Err(e) = stream.write_all(&buf[..n]) {
+                    eprintln!("Error writing to client {}: {}", addr, e);
+                    break;
+                }
+            }
+        });
+    }
+
+    listener.close()?;
+}

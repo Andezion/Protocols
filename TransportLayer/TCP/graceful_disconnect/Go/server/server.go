2fec63063fb4f78f914bef53a1123c5329bbbe44
Andezion <245122@edu.p.lodz.pl>
2026-03-20 18:05:37 +0100

    add: statred go graceful close

--- TransportLayer/TCP/graceful_disconnect/Go/server/server.go
+++ TransportLayer/TCP/graceful_disconnect/Go/server/server.go
@@ -13,4 +13,34 @@ func main() {
 	}
 
 	fmt.Println("Server is listening on port 8090...")
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection:", err)
+			continue
+		}
+
+		fmt.Printf("Client connected: %s\n", conn.RemoteAddr().String())
+
+		go func(c net.Conn) {
+			defer c.Close() // гарантируем, что соединение будет закрыто после обработки клиента
+
+			buf := make([]byte, 1024)
+			n, err := c.Read(buf)
+			if err != nil {
+				fmt.Println("Error reading from client:", err)
+				return
+			}
+
+			fmt.Printf("Received from client %s: %s\n", c.RemoteAddr().String(), string(buf[:n]))
+			_, err = c.Write(buf[:n])
+			if err != nil {
+				fmt.Println("Error writing to client:", err)
+				return
+			}
+
+			fmt.Printf("Echoed back to client %s.\n", c.RemoteAddr().String())
+		}(conn)
+	}
 }

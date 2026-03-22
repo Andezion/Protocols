7caf4ea80c7925638b38dab1d97bb39632473682
Andezion <245122@edu.p.lodz.pl>
2026-03-22 23:04:39 +0100

    add: started cool server in Go

--- TransportLayer/UDP/multiple_clients/Go/server/server.go
+++ TransportLayer/UDP/multiple_clients/Go/server/server.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"log"
 	"net"
 )
@@ -11,18 +13,31 @@ func main() {
 		log.Fatal(err)
 	}
 
+	fmt.Println("UDP server is listening on port 8090...")
+
 	for {
 		buf := make([]byte, 2048)
+
 		n, addr, err := listener.ReadFrom(buf)
 		if err != nil {
 			log.Println("read error:", err)
 			continue
 		}
 
-		log.Printf("Received %d bytes from %s: %s\n", n, addr.String(), string(buf[:n]))
-		_, err = listener.WriteTo(buf[:n], addr)
-		if err != nil {
-			log.Println("write error:", err)
-		}
+		fmt.Printf("Received %d bytes from %s: %s\n", n, addr.String(), string(buf[:n]))
+
+		go func(data []byte, clientAddr net.Addr) {
+			scanner := bufio.NewScanner(bufio.NewReaderSize(nil, len(data)))
+			scanner.Split(bufio.ScanLines)
+
+			for scanner.Scan() {
+				line := scanner.Text()
+				fmt.Printf("Processing line from %s: %s\n", clientAddr.String(), line)
+				_, err := listener.WriteTo([]byte(line), clientAddr)
+				if err != nil {
+					log.Println("write error:", err)
+				}
+			}
+		}(buf[:n], addr)
 	}
 }

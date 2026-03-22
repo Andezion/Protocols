88e48e4cf695860108e230d186c6cb2896ee7842
Andezion <245122@edu.p.lodz.pl>
2026-03-22 23:02:31 +0100

    add: started cool server in Go

--- TransportLayer/UDP/multiple_clients/Go/server/server.go
+++ TransportLayer/UDP/multiple_clients/Go/server/server.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net"
+)
 
+func main() {
+	listener, err := net.ListenPacket("udp", ":8090")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer listener.Close()
 }

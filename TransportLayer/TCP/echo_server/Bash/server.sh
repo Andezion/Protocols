0e1f1bc53f05cc05ecac55fbf610e79ec8a8d42f
Andezion <245122@edu.p.lodz.pl>
2026-03-24 00:26:14 +0100

    fix: fixing

--- TransportLayer/TCP/echo_server/Bash/server.sh
+++ TransportLayer/TCP/echo_server/Bash/server.sh
@@ -3,7 +3,7 @@
 echo "Starting TCP echo server on port 12345..."
 while true; do
     # Listen for incoming connections on port 12345
-    nc -l -p 12345 -c 'cat'
+    nc -l -p 12345 -c 'cat' || sleep 1
 done
 
     

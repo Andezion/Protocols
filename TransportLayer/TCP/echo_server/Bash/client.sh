3e69bf48ad7fc75f284a5af79f64426a5436008f
Andezion <245122@edu.p.lodz.pl>
2026-03-24 00:24:33 +0100

    fix: fixing server

--- TransportLayer/TCP/echo_server/Bash/client.sh
+++ TransportLayer/TCP/echo_server/Bash/client.sh
@@ -6,6 +6,6 @@ SERVER_PID=$!
 sleep 1
 # Connect to the TCP echo server and send a message
 echo "Hello, TCP Echo Server!" | nc localhost 12345
-# Clean up: kill the server process
-kill $SERVER_PID
+# Clean up: kill the server and all its child processes
+kill -- -$(ps -o pgid= -p $SERVER_PID | tr -d ' ') 2>/dev/null
 
--- TransportLayer/TCP/echo_server/Bash/server.sh
+++ TransportLayer/TCP/echo_server/Bash/server.sh
@@ -3,7 +3,7 @@
 echo "Starting TCP echo server on port 12345..."
 while true; do
     # Listen for incoming connections on port 12345
-    nc -l -p 12345 -c 'xargs -n1 echo'
+    nc -l -p 12345 -c 'cat'
 done
 
     

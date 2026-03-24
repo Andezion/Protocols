73dbd740d31b8749635056fa2c0c52c99bbe75d9
Andezion <245122@edu.p.lodz.pl>
2026-03-24 11:41:42 +0100

    add: started zig tcp stuff

--- TransportLayer/TCP/echo_server/Zig/client.zig
+++ TransportLayer/TCP/echo_server/Zig/client.zig
@@ -1,3 +1,11 @@
 const std = @import("std");
 
-pub fn main() !void {}
+pub fn main() !void {
+    const allocator = std.heap.page_allocator;
+    const stdout = std.io.getStdOut().writer();
+
+    const address = try std.net.Address.parseIp4("127.0.0.1:12345");
+    const socket = try std.net.StreamSocket(.{});
+    defer socket.close();
+    try socket.connect(address);
+}

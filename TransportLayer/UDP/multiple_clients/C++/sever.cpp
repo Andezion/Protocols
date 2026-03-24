e07f28aff81e9def553a969cc77d1ab55df36b84
Andezion <245122@edu.p.lodz.pl>
2026-03-24 17:52:40 +0100

    add: started udp c++

--- TransportLayer/UDP/multiple_clients/C++/sever.cpp
+++ TransportLayer/UDP/multiple_clients/C++/sever.cpp
@@ -6,5 +6,8 @@
 using boost::asio::ip::udp;
 
 int main() {
-    
+    boost::asio::io_context io_context;
+    udp::socket socket(io_context, udp::endpoint(udp::v4(), 8090)); 
+
+    std::cout << "UDP Server is running on port 8090..." << std::endl;
 }

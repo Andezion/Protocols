1745f7cf8b8e8d08d7557e5022627862eca9edbb
Andezion <245122@edu.p.lodz.pl>
2026-03-24 16:38:31 +0100

    fix: fixing stuff

--- TransportLayer/UDP/echo_server/Zig/client.zig
+++ TransportLayer/UDP/echo_server/Zig/client.zig
@@ -1,13 +1,16 @@
 const std = @import("std");
+const posix = std.posix;
 
 pub fn main() !void {
-    const address = try std.net.Address.resolveIp("127.0.0.1", 12345);
-    const stream = try std.net.tcpConnectToAddress(address);
-    defer stream.close();
+    const sock = try posix.socket(posix.AF.INET, posix.SOCK.DGRAM, 0);
+    defer posix.close(sock);
 
-    try stream.writeAll("Hello from client!\n");
+    const address = try std.net.Address.resolveIp("127.0.0.1", 12345);
+
+    const msg = "Hello from client!\n";
+    _ = try posix.sendto(sock, msg, 0, &address.any, address.getOsSockLen());
 
     var buf: [1024]u8 = undefined;
-    const n = try stream.read(&buf);
+    const n = try posix.recvfrom(sock, &buf, 0, null, null);
     std.debug.print("{s}", .{buf[0..n]});
 }

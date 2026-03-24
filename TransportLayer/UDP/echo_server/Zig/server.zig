2d838644dd6f45e8c9d6772b4c2bca5cb5458fc1
Andezion <245122@edu.p.lodz.pl>
2026-03-24 12:55:22 +0100

    add: started zig echo server

--- TransportLayer/UDP/echo_server/Zig/server.zig
+++ TransportLayer/UDP/echo_server/Zig/server.zig
@@ -1,1 +1,19 @@
 const std = @import("std");
+
+pub fn main() !void {
+    const address = try std.net.Address.resolveIp("127.0.0.1", 12345);
+    var server = try address.listen(.{ .reuse_address = true });
+    defer server.deinit();
+
+    std.debug.print("UDP echo server listening on port 12345...\n", .{});
+
+    while (true) {
+        var buf: [1024]u8 = undefined;
+        const n = try server.recvFrom(&buf);
+        if (n == 0) continue;
+
+        const msg = buf[0..n];
+        std.debug.print("Client says: {s}", .{msg});
+        try server.sendTo(msg, address);
+    }
+}

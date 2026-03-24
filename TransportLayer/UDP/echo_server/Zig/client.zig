5e3c32f315417ca4f64155642cb8aa5d5ad91d24
Andezion <245122@edu.p.lodz.pl>
2026-03-24 16:30:08 +0100

    add: added upd stuff

--- TransportLayer/UDP/echo_server/Zig/client.zig
+++ TransportLayer/UDP/echo_server/Zig/client.zig
@@ -1,1 +1,13 @@
 const std = @import("std");
+
+pub fn main() !void {
+    const address = try std.net.Address.resolveIp("127.0.0.1", 12345);
+    const stream = try std.net.tcpConnectToAddress(address);
+    defer stream.close();
+
+    try stream.writeAll("Hello from client!\n");
+
+    var buf: [1024]u8 = undefined;
+    const n = try stream.read(&buf);
+    std.debug.print("{s}", .{buf[0..n]});
+}

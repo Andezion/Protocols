245cd10c66ad3a448a9982605ce60c6e9c202ff9
Andezion <245122@edu.p.lodz.pl>
2026-03-24 17:59:50 +0100

    add: started udp c++

--- TransportLayer/UDP/multiple_clients/C++/client.cpp
+++ TransportLayer/UDP/multiple_clients/C++/client.cpp
@@ -1,18 +1,19 @@
+#include <boost/asio/ip/udp.hpp>
 #include <boost/asio/streambuf.hpp>
 #include <string>
 #include <boost/asio.hpp>
 #include <iostream>
 
-using boost::asio::ip::tcp;
+using boost::asio::ip::udp;
 
 int main() {
     // Создаем io_context и resolver для разрешения адреса сервера
     boost::asio::io_context io_context;
-    tcp::resolver resolver(io_context);
+    udp::resolver resolver(io_context);
 
     // Разрешаем адрес сервера и порт
     auto endpoints = resolver.resolve("127.0.0.1", "8090");
-    tcp::socket socket(io_context);
+    udp::socket socket(io_context);
 
     // Подключаемся к серверу
     boost::asio::connect(socket, endpoints);

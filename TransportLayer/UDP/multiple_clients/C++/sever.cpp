4c24c1771a8a26582f0404d9429943bc04631c3a
Andezion <245122@edu.p.lodz.pl>
2026-03-24 17:53:56 +0100

    add: started udp c++

--- TransportLayer/UDP/multiple_clients/C++/sever.cpp
+++ TransportLayer/UDP/multiple_clients/C++/sever.cpp
@@ -10,4 +10,28 @@ int main() {
     udp::socket socket(io_context, udp::endpoint(udp::v4(), 8090)); 
 
     std::cout << "UDP Server is running on port 8090..." << std::endl;
+
+    char data[1024];
+    udp::endpoint sender_endpoint;
+    boost::system::error_code ec;
+
+    while (true) {
+        size_t length = socket.receive_from(boost::asio::buffer(data), sender_endpoint, 0, ec);
+        if (ec) {
+            std::cerr << "Receive error: " << ec.message() << std::endl;
+            continue; // Продолжать принимать следующие сообщения
+        }
+
+        std::string received(data, length);
+        std::cout << "Received from client: " << sender_endpoint.address().to_string() << ":" << sender_endpoint.port() << " " << received << std::endl;
+
+        std::string message = "Hello from server!";
+        socket.send_to(boost::asio::buffer(message), sender_endpoint, 0, ec);
+        if (ec) {
+            std::cerr << "Send error: " << ec.message() << std::endl;
+            continue; // Продолжать принимать следующие сообщения
+        }
+    }
+
+    return 0;
 }

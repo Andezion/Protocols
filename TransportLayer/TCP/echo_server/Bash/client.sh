fe0983936d90602b697075c2a5ea591b13e52fc4
Andezion <245122@edu.p.lodz.pl>
2026-03-24 10:44:20 +0100

    fix: fixing client

--- TransportLayer/TCP/echo_server/Bash/client.sh
+++ TransportLayer/TCP/echo_server/Bash/client.sh
@@ -1,11 +1,4 @@
 #!/usr/bin/env bash
 
-./server.sh &
-SERVER_PID=$!
-# Give the server a moment to start
-sleep 1
-# Connect to the TCP echo server and send a message
 echo "Hello, TCP Echo Server!" | nc localhost 12345
-# Clean up: kill the server and all its child processes
-kill -- -$(ps -o pgid= -p $SERVER_PID | tr -d ' ') 2>/dev/null
 

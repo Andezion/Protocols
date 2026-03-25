8061fc19280b4ca29bc45b300dc942e35ed12a61
Andezion <245122@edu.p.lodz.pl>
2026-03-25 17:46:03 +0100

    fix: added text

--- TransportLayer/UDP/echo_server/Zig/server.zig
+++ TransportLayer/UDP/echo_server/Zig/server.zig
@@ -2,10 +2,15 @@ const std = @import("std");
 const posix = std.posix;
 
 pub fn main() !void {
+    // Создваем UDP сокет. AF.INET - IPv4, SOCK.DGRAM - UDP, 0 - протокол по умолчанию
     const sock = try posix.socket(posix.AF.INET, posix.SOCK.DGRAM, 0);
+    // Закрываем сокет при выходе из функции
     defer posix.close(sock);
 
+    // Привязываем сокет к адресу и порту
     const address = try std.net.Address.resolveIp("127.0.0.1", 12345);
+
+    // Связываем сокет с адресом и портом, чтобы сервер мог принимать данные
     try posix.bind(sock, &address.any, address.getOsSockLen());
 
     std.debug.print("UDP echo server listening on port 12345...\n", .{});

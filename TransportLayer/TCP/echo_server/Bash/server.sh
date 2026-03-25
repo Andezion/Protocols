f57a7fb0949bc685bba12df52d4680e95de1697f
Andezion <245122@edu.p.lodz.pl>
2026-03-25 17:18:10 +0100

    fix: added text

--- TransportLayer/TCP/echo_server/Bash/server.sh
+++ TransportLayer/TCP/echo_server/Bash/server.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
 echo "Starting TCP echo server on port 12345..."
+# Бесконечный цикл для обработки входящих соединений
 while true; do
+    # Используем netcat для прослушивания порта 12345 и обработки входящих сообщений
+    # -l - слушать входящие соединения
+    # -p 12345 - указать порт для прослушивания
+    # -c - выполнить команду для каждого входящего соединения
+    #  while IFS= read -r line; do ... done - цикл для чтения входящих сообщений от клиента
     nc -l -p 12345 -c 'while IFS= read -r line; do echo "Client says: $line" >&2; echo "Hello from server!"; done'
 done
 

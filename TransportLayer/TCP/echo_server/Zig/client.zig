7fb1b63b39688c4d01971da0d1ea8c7aa467e84e
Andezion <245122@edu.p.lodz.pl>
2026-03-25 17:28:30 +0100

    fix: added text

--- TransportLayer/TCP/echo_server/Zig/client.zig
+++ TransportLayer/TCP/echo_server/Zig/client.zig
@@ -5,11 +5,17 @@ pub fn main() !void {
     const address = try std.net.Address.resolveIp("127.0.0.1", 12345);
     const stream = try std.net.tcpConnectToAddress(address); // тут мы создаем TCP-соединение с сервером
 
+    // Закрываем соединение при выходе из функции
     defer stream.close();
 
+    // Отправляем сообщение серверу
     try stream.writeAll("Hello from client!\n");
 
-    var buf: [1024]u8 = undefined;
-    const n = try stream.read(&buf);
+    var buf: [1024]u8 = undefined; // тут мы создаем буфер для чтения данных от сервера
+    const n = try stream.read(&buf); // тут мы читаем данные от сервера в буфер и сохраняем количество прочитанных байт в переменной n
+    if (n == 0) {
+        std.debug.print("Server closed the connection.\n", .{});
+        return;
+    }
     std.debug.print("{s}", .{buf[0..n]});
 }

1c6d9c55d0754992be35ca0c38556dcbe5cc5837
Andezion <245122@edu.p.lodz.pl>
2026-03-26 16:46:07 +0100

    add: started mqtt go impl

--- ApplicationLayer/MQTT/Go/client/client.go
+++ ApplicationLayer/MQTT/Go/client/client.go
@@ -1,3 +1,20 @@
 package main
 
 import (
+	"fmt"
+	"sync"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+)
+
+func main() {
+	opts := mqtt.NewClientOptions().AddBroker("tcp://localhost:1883").SetClientID("go_mqtt_client")
+	client := mqtt.NewClient(opts)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+	fmt.Println("Connected to MQTT broker")
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+}

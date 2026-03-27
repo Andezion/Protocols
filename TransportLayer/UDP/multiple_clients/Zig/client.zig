fccbfc5642c6833c9d5d0c8850255c40bb567778
Andezion <245122@edu.p.lodz.pl>
2026-03-27 22:27:41 +0100

    add: added more zig

--- TransportLayer/UDP/multiple_clients/Zig/client.zig
+++ TransportLayer/UDP/multiple_clients/Zig/client.zig
@@ -1,4 +1,7 @@
 const std = @import("std");
 const posix = std.posix;
 
-pub fn main() !void {}
+pub fn main() !void {
+    const sock = try posix.socket(posix.AF.INET, posix.SOCK.DGRAM, 1);
+    defer posix.close(sock);
+}

e4161a0fce54f19254868f1ebbdf19b5ff66e8cd
Andezion <245122@edu.p.lodz.pl>
2026-03-27 22:29:33 +0100

    add: added more zig

--- TransportLayer/UDP/multiple_clients/Zig/client.zig
+++ TransportLayer/UDP/multiple_clients/Zig/client.zig
@@ -4,4 +4,12 @@ const posix = std.posix;
 pub fn main() !void {
     const sock = try posix.socket(posix.AF.INET, posix.SOCK.DGRAM, 1);
     defer posix.close(sock);
+
+    const address = try std.net.Address.resolveIp("127.0.0.1", 8080);
+
+    const msg = "Hello from client!\n";
+    _ = try posix.sendto(sock, msg, 0, &address.any, address.getOsSockLen());
+    var buf: [1024]u8 = undefined;
+    const n = try posix.recvfrom(sock, &buf, 0, null, null);
+    std.debug.print("{s}", .{buf[0..n]});
 }

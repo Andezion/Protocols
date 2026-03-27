43b232b79dcc95ac7db800db55426945cf0b30fa
Andezion <245122@edu.p.lodz.pl>
2026-03-27 22:33:14 +0100

    add: added more zig

--- TransportLayer/UDP/multiple_clients/Zig/server.zig
+++ TransportLayer/UDP/multiple_clients/Zig/server.zig
@@ -1,4 +1,11 @@
 const std = @import("std");
 const posix = std.posix;
 
-pub fn main() !void {}
+pub fn main() !void {
+    const sock = try posix.socket(posix.AF.INET, posix.SOCK_DGRAM, 1);
+    defer posix.close(sock);
+
+    const address = try std.net.Address.resolveIp("127.0.0.1", 8080);
+    _ = try posix.bind(sock, &address.any, address.getOsSockLen());
+    std.debug.print("UDP server listening on port 8080...\n", .{});
+}

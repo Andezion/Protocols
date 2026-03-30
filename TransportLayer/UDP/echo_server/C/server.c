360b63b319eaf372a4de07f8f07dc11979fe2cf9
Andezion <245122@edu.p.lodz.pl>
2026-03-30 16:26:35 +0200

    add: working on udp

--- TransportLayer/UDP/echo_server/C/server.c
+++ TransportLayer/UDP/echo_server/C/server.c
@@ -16,28 +16,27 @@ int main() {
     }
 
     printf("UDP server is listening on port %u\n", port);
-char buffer[UDP_MAX_PAYLOAD];
-        struct sockaddr_in client_addr;
-        socklen_t client_addr_len = sizeof(client_addr);
+    char buffer[UDP_MAX_PAYLOAD];
+    struct sockaddr_in client_addr;
+    socklen_t client_addr_len = sizeof(client_addr);
 
-        // Получаем данные от клиента
-        ssize_t recv_bytes = udp_recvfrom(sockfd, buffer, sizeof(buffer) - 1, (struct sockaddr *)&client_addr, &client_addr_len);
-        if (recv_bytes < 0) {
-            fprintf(stderr, "Failed to receive message from client.\n");
-        }
+    // Получаем данные от клиента
+    ssize_t recv_bytes = udp_recvfrom(sockfd, buffer, sizeof(buffer) - 1, (struct sockaddr *)&client_addr, &client_addr_len);
+    if (recv_bytes < 0) {
+        fprintf(stderr, "Failed to receive message from client.\n");
+    }
 
-        buffer[recv_bytes] = '\0'; // Добавляем нулевой терминатор к полученному сообщению
-        printf("Received message from client: %s\n", buffer);
+    buffer[recv_bytes] = '\0'; // Добавляем нулевой терминатор к полученному сообщению
+    printf("Received message from client: %s\n", buffer);
 
-        // Отправляем ответ клиенту
-        const char *reply = "Hello from UDP server!";
-        ssize_t sent_bytes = udp_sendto(sockfd, reply, strlen(reply), (struct sockaddr *)&client_addr, client_addr_len);
-        if (sent_bytes < 0) {
-            fprintf(stderr, "Failed to send message to client.\n");
-            continue; // продолжаем слушать других клиентов
-        }
+    // Отправляем ответ клиенту
+    const char *reply = "Hello from UDP server!";
+    ssize_t sent_bytes = udp_sendto(sockfd, reply, strlen(reply), (struct sockaddr *)&client_addr, client_addr_len);
+    if (sent_bytes < 0) {
+        fprintf(stderr, "Failed to send message to client.\n");
+    }
 
-    // Закрываем сокет (хотя в этом примере мы никогда не достигнем этой строки)
+// Закрываем сокет (хотя в этом примере мы никогда не достигнем этой строки)
     close(sockfd);
     return EXIT_SUCCESS;
 }

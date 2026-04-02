f6770f1094e8520e39b9c566ccffa9d46d24d5e8
Andezion <245122@edu.p.lodz.pl>
2026-04-02 11:33:03 +0200

    add: added sctp

--- TransportLayer/SCTP/echo_server/Rust/client.rs
+++ TransportLayer/SCTP/echo_server/Rust/client.rs
@@ -2,8 +2,17 @@ use std::io::{self, Read, Write};
 use std::net::TcpStream;
 
 fn main() -> std::io::Result<()> {
-    
+    let addr = "127.0.0.1:8090";
+    let mut stream = TcpStream::connect(addr)?;
+    println!("Connected to server at {}", addr);
 
+    let msg = b"Hello from Rust client\n";
+    stream.write_all(msg)?; // Отправляем сообщение серверу
+
+    // Читаем ответ от сервера
+    let mut buf = [0u8; 1024];
+    let n = stream.read(&mut buf)?;
+    println!("Received echo: {}", String::from_utf8_lossy(&buf[..n]));
 
     Ok(())
 }
--- TransportLayer/SCTP/echo_server/Rust/server.rs
+++ TransportLayer/SCTP/echo_server/Rust/server.rs
@@ -4,10 +4,10 @@ use std::net::TcpListener;
 fn main() -> std::io::Result<()> {
     let client = sctp_rs::Socket::new_v4(sctp_rs::SocketToAssociation::OneToOne)?;
 
-    client.bind("127.0.0.1:3000")?;
+    client.bind("127.0.0.1:8090")?;
     client.listen(1)?;
-    
-    println!("SCTP echo server listening on 3000 port");
+
+    println!("SCTP echo server listening on 8090 port");
 
     let (mut stream, addr) = client.accept()?;
     println!("Client connected: {}", addr);

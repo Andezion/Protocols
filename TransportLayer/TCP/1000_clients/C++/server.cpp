5ad2c1ec63ba15921245f9a4409f9f31fa6c937a
Andezion <245122@edu.p.lodz.pl>
2026-04-12 01:14:27 +0200

    add: added new stuff

---
+++ TransportLayer/TCP/1000_clients/C++/server.cpp
@@ -0,0 +1,32 @@
+#include <string>
+#include <boost/asio.hpp>
+#include <iostream>
+
+using boost::asio::ip::tcp;
+
+int main() {
+    // Создаем io_context и acceptor для прослушивания входящих соединений на порту 8090
+    boost::asio::io_context io_context;
+    tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(),8090));
+    
+    std::cout << "Server is running on port 8090..." << std::endl;
+
+    // Ожидаем входящее соединение и принимаем его
+    tcp::socket socket(io_context);
+    acceptor.accept(socket);
+        
+    std::string message = "Hello from the server!";
+
+    // Читаем данные от клиента до символа новой строки
+    boost::asio::streambuf buffer;
+    boost::asio::read_until(socket, buffer, '\n');
+
+    // Преобразуем полученные данные в строку и выводим на консоль
+    std::string client_msg{buffers_begin(buffer.data()), buffers_end(buffer.data())};
+    std::cout << "Received from client: " << client_msg << std::endl;
+
+    // Отправляем ответ клиенту
+    boost::asio::write(socket, boost::asio::buffer(message + "\n"));
+
+    return 0;
+}

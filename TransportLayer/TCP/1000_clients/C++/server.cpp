cfb55c43743d4fb99163678f62bd3963f5467ee1
Andezion <245122@edu.p.lodz.pl>
2026-04-12 12:53:30 +0200

    add: added cool server

--- TransportLayer/TCP/1000_clients/C++/server.cpp
+++ TransportLayer/TCP/1000_clients/C++/server.cpp
@@ -5,28 +5,26 @@
 using boost::asio::ip::tcp;
 
 int main() {
-    // Создаем io_context и acceptor для прослушивания входящих соединений на порту 8090
     boost::asio::io_context io_context;
     tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(),8090));
-    
+
     std::cout << "Server is running on port 8090..." << std::endl;
 
-    // Ожидаем входящее соединение и принимаем его
-    tcp::socket socket(io_context);
-    acceptor.accept(socket);
-        
-    std::string message = "Hello from the server!";
+    // и вот тут единственная разница - у нас бесконечный цикл
+    while (true) {
+        tcp::socket socket(io_context);
+        acceptor.accept(socket);
 
-    // Читаем данные от клиента до символа новой строки
-    boost::asio::streambuf buffer;
-    boost::asio::read_until(socket, buffer, '\n');
+        std::string message = "Hello from the server!";
 
-    // Преобразуем полученные данные в строку и выводим на консоль
-    std::string client_msg{buffers_begin(buffer.data()), buffers_end(buffer.data())};
-    std::cout << "Received from client: " << client_msg << std::endl;
+        boost::asio::streambuf buffer;
+        boost::asio::read_until(socket, buffer, '\n');
 
-    // Отправляем ответ клиенту
-    boost::asio::write(socket, boost::asio::buffer(message + "\n"));
+        std::string client_msg{buffers_begin(buffer.data()), buffers_end(buffer.data())};
+        std::cout << "Received from client: " << client_msg << std::endl;
 
-    return 0;
+        boost::asio::write(socket, boost::asio::buffer(message + "\n"));
+    }
+
+    return 0;   
 }

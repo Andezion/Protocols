3f014017f394c44afeeb7a507159b667abaaaf8c
Andezion <245122@edu.p.lodz.pl>
2026-04-12 13:52:46 +0200

    add: working with class

--- TransportLayer/TCP/1000_clients/C++/client.cpp
+++ TransportLayer/TCP/1000_clients/C++/client.cpp
@@ -5,6 +5,20 @@
 
 using boost::asio::ip::tcp;
 
+class Client {
+public:
+    Client() {
+        // Создаем io_context и resolver для разрешения адреса сервера
+        boost::asio::io_context io_context;
+        tcp::resolver resolver(io_context);
+
+        // Разрешаем адрес сервера и порт
+        auto endpoints = resolver.resolve("127.0.0.1", "8090");
+        tcp::socket socket(io_context);
+    }
+
+};
+
 int main() {
     int i = 0;
     while (i < 1000) {

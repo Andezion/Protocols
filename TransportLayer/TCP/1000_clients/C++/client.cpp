a57c877445ce06d0a1adab1fca0b43f5a5d36e71
Andezion <245122@edu.p.lodz.pl>
2026-04-12 13:54:27 +0200

    add: working with class

--- TransportLayer/TCP/1000_clients/C++/client.cpp
+++ TransportLayer/TCP/1000_clients/C++/client.cpp
@@ -19,7 +19,17 @@ public:
         tcp::socket socket(io_context);
 
         boost::asio::connect(socket, endpoints);
-        std::string message = "Hello from the client!\n";
+        std::string message = "Hello from the client " + std::to_string(id) + "\n";
+
+        boost::asio::write(socket, boost::asio::buffer(message));
+
+        // Читаем ответ от сервера до символа новой строки
+        boost::asio::streambuf response;
+        boost::asio::read_until(socket, response, '\n');
+
+        // Преобразуем полученные данные в строку и выводим на консоль
+        std::string client_msg{buffers_begin(response.data()), buffers_end(response.data())};
+        std::cout << "Received from server: " << client_msg << std::endl;
     }
 
 };

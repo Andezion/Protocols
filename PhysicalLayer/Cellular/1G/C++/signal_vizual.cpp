851ecdbcce79aebe0a8ce1adf5ded59f4980e576
Andezion <vdoronchenkov@gmail.com>
2026-05-11 23:12:55 +0200

    add: stuff to signal stuff to 1g

---
+++ PhysicalLayer/Cellular/1G/C++/signal_vizual.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+
+void fm_modulate_signal() {
+
+}
+
+void rule_of_karson() {
+
+}
+
+void generator_of_signal() {
+
+}
+
+void sheme_of_offset() {
+
+}
+
+
+
+int main() {
+    
+    return 0;
+}

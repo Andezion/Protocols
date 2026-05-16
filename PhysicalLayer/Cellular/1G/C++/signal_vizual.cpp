4d03565fe5b0e41c2a3f23ebf33d18b933300129
Andezion <vdoronchenkov@gmail.com>
2026-05-16 11:52:59 +0200

    add: added modulate signal

--- PhysicalLayer/Cellular/1G/C++/signal_vizual.cpp
+++ PhysicalLayer/Cellular/1G/C++/signal_vizual.cpp
@@ -5,14 +5,25 @@
 #define fc 1000.0 // частота несущей
 #define fic 0.0 // начальная фаза несущей
 
+#define kf 100.0 // коэффициент частотной модуляции
+
+// тут считаем интеграл от функции func от low до high
+float integral(int high, int low, float (*func)(int)) {
+    float sum = 0.0;
+    for (int i = low; i <= high; i++) {
+        sum += func(i);
+    }
+    return sum;
+}
+
 // тут мы будем описывать математическую несущую
 float mathematically_load_bearing(int t) {
     return Ac * cos(2 * M_PI * fc * t + fic);
 }
 
 // тут мы будем описывать фм модулированный сигнал 
-void fm_modulate_signal() {
-
+float fm_modulate_signal(int t) {
+    return Ac * cos(2 * M_PI * fc * t + 2 * M_PI * kf * integral(t, 0, m(tau)));
 }
 
 // тут мы будем описывать правило Карсона для оценки ширины спектра фм сигнала

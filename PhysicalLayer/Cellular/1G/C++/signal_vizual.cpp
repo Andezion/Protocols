48954ce7af75c8cd3f4ba6e53688efb1c80b7062
Andezion <vdoronchenkov@gmail.com>
2026-05-19 19:24:28 +0200

    fix: trying to fix raw data

--- PhysicalLayer/Cellular/1G/C++/signal_vizual.cpp
+++ PhysicalLayer/Cellular/1G/C++/signal_vizual.cpp
@@ -106,9 +106,9 @@ void draw_mesh() {
                 (Vector2){ 800 + 70 + 8, 400 + 80 + 5 },
                 (Vector2){ 800 + 70 + 8, 400 + 90 + 5 }, BLACK);
 
-    DrawTriangle((Vector2){static_cast<float>(screenWidth) / 10 * 8},
-                (Vector2){static_cast<float>(screenWidth) / 10 * 8}, 
-                (Vector2){static_cast<float>(screenWidth) / 10 * 8});
+    DrawTriangle((Vector2){static_cast<float>(screenWidth) / 10 * 8 + static_cast<float>(screenWidth) / 100 * 9, static_cast<float>(screenHeight) / 6 * 4 + },
+                (Vector2){static_cast<float>(screenWidth) / 10 * 8 + static_cast<float>(screenWidth) / 100 * 7, static_cast<float>(screenHeight) / 6 * 4 + }, 
+                (Vector2){static_cast<float>(screenWidth) / 10 * 8 + static_cast<float>(screenWidth) / 100 * 7, static_cast<float>(screenHeight) / 6 * 4 + });
 
     for (int i = 1; i <= 67; i++) {
         DrawLine(200 + 10 + i * 10, 400 + 80 + 7, 210 + i * 10, 400 + 90 + 2, BLACK);

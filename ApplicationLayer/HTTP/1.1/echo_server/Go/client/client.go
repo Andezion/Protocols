f7ff91c298e8d133482af3c173b21617c94f8bb7
Andezion <vdoronchenkov@gmail.com>
2026-05-21 12:25:04 +0200

    add: playing with client

--- ApplicationLayer/HTTP/1.1/echo_server/Go/client/client.go
+++ ApplicationLayer/HTTP/1.1/echo_server/Go/client/client.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"os"
+	"os/exec"
+)
 
+func main() {
+	cmd := exec.Command("ls", "-la")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
+		panic(err)
+	}
 }

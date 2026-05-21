16bc82047623f5bfe78974c877a653882ffe2588
Andezion <vdoronchenkov@gmail.com>
2026-05-21 13:46:36 +0200

    add: started diff pick for client

--- ApplicationLayer/HTTP/1.1/echo_server/Go/client/client.go
+++ ApplicationLayer/HTTP/1.1/echo_server/Go/client/client.go
@@ -6,10 +6,19 @@ import (
 )
 
 func main() {
-	cmd := exec.Command("curl", "http://localhost:8080/echo", "-X", "POST", "-d", "Hello, Echo Server!\n")
+
+	cmd := exec.Command("echo", "Hello User, to test echo server click 1, to test ping server click 2")
 	cmd.Stdout = os.Stdout
 
 	if err := cmd.Run(); err != nil {
 		panic(err)
 	}
+
+	// cmd := exec.Command("curl", "http://localhost:8080/echo", "-X", "POST", "-d", "Hello, Echo Server!\n")
+	// cmd.Stdout = os.Stdout
+
+	// if err := cmd.Run(); err != nil {
+	// 	panic(err)
+	// }
+
 }

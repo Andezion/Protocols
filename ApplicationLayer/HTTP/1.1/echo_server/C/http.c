ccb1141f78e493f94221123a6c8515b26f38d380
Andezion <vdoronchenkov@gmail.com>
2026-05-24 20:42:00 +0200

    fix: fixed import issue

--- ApplicationLayer/HTTP/1.1/echo_server/C/http.c
+++ ApplicationLayer/HTTP/1.1/echo_server/C/http.c
@@ -1,11 +1,37 @@
 #include "http.h"
 
-http_responce http_get(const char *url, struct http_request *request);
-http_responce http_post(const char *url, const char *body, struct http_request *request);
-http_responce http_put(const char *url, const char *body, struct http_request *request);
-http_responce http_delete(const char *url, struct http_request *request);
-http_responce http_head(const char *url, struct http_request *request);
-http_responce http_options(const char *url, struct http_request *request);
-http_responce http_patch(const char *url, const char *body, struct http_request *request);
-http_responce http_trace(const char *url, struct http_request *request);
-http_responce http_connect(const char *url, struct http_request *request);
+struct http_responce http_get(const char *url, struct http_request *request) {
+
+}
+
+struct http_responce http_post(const char *url, const char *body, struct http_request *request) {
+    
+}
+
+struct http_responce http_put(const char *url, const char *body, struct http_request *request) {
+    
+}
+
+struct http_responce http_delete(const char *url, struct http_request *request) {
+    
+}
+
+struct http_responce http_head(const char *url, struct http_request *request) {
+    
+}
+
+struct http_responce http_options(const char *url, struct http_request *request) {
+    
+}
+
+struct http_responce http_patch(const char *url, const char *body, struct http_request *request) {
+
+}
+
+struct http_responce http_trace(const char *url, struct http_request *request) {
+
+}
+
+struct http_responce http_connect(const char *url, struct http_request *request) {
+
+}

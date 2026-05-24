a73a4b85be4265ccadce671af1d18c70c1648d3a
Andezion <vdoronchenkov@gmail.com>
2026-05-24 20:50:57 +0200

    add: working on http.c

--- ApplicationLayer/HTTP/1.1/echo_server/C/http.c
+++ ApplicationLayer/HTTP/1.1/echo_server/C/http.c
@@ -1,37 +1,86 @@
 #include "http.h"
+#include <string.h>
 
 struct http_responce http_get(const char *url, struct http_request *request) {
-
+    request->method = GET;
+    request->url = strdup(url);
+    request->version = strdup("HTTP/1.1");
+    request->hearders = NULL;
+    request->body = strdup("Hello bitch");
+    struct http_responce responce;
 }
 
 struct http_responce http_post(const char *url, const char *body, struct http_request *request) {
-    
+    request->method = POST;
+    request->url = strdup(url);
+    request->version = strdup("HTTP/1.1");
+    request->hearders = NULL;
+    request->body = strdup(body);
+    struct http_responce responce;
 }
 
 struct http_responce http_put(const char *url, const char *body, struct http_request *request) {
-    
+    request->method = PUT;
+    request->url = strdup(url);
+    request->version = strdup("HTTP/1.1");
+    request->hearders = NULL;
+    request->body = strdup(body);
+    struct http_responce responce;
 }
 
 struct http_responce http_delete(const char *url, struct http_request *request) {
-    
+    request->method = DELETE;
+    request->url = strdup(url);
+    request->version = strdup("HTTP/1.1");
+    request->hearders = NULL;
+    request->body = NULL;
+    struct http_responce responce;
 }
 
 struct http_responce http_head(const char *url, struct http_request *request) {
-    
+    request->method = HEAD;
+    request->url = strdup(url);
+    request->version = strdup("HTTP/1.1");
+    request->hearders = NULL;
+    request->body = NULL;
+    struct http_responce responce;
 }
 
 struct http_responce http_options(const char *url, struct http_request *request) {
-    
+    struct http_responce responce = {
+        .version = strdup("HTTP/1.1"),
+        .status_code = HTTP_OK,
+        .status_message = strdup("OK"),
+        .headers = strdup("Allow: GET, POST, PUT, DELETE, HEAD, OPTIONS, PATCH, TRACE, CONNECT\r\n"),
+        .body = NULL
+    };
+
+    return responce;
 }
 
 struct http_responce http_patch(const char *url, const char *body, struct http_request *request) {
-
+    request->method = PATCH;
+    request->url = strdup(url);
+    request->version = strdup("HTTP/1.1");
+    request->hearders = NULL;
+    request->body = strdup(body);
+    struct http_responce responce;
 }
 
 struct http_responce http_trace(const char *url, struct http_request *request) {
-
+    request->method = TRACE;
+    request->url = strdup(url);
+    request->version = strdup("HTTP/1.1");
+    request->hearders = NULL;
+    request->body = NULL;
+    struct http_responce responce;
 }
 
 struct http_responce http_connect(const char *url, struct http_request *request) {
-
+    request->method = CONNECT;
+    request->url = strdup(url);
+    request->version = strdup("HTTP/1.1");
+    request->hearders = NULL;
+    request->body = NULL;
+    struct http_responce responce;
 }

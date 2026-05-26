cc093be68c0ecbe1651c0ac24fab12121baa9f04
Andezion <vdoronchenkov@gmail.com>
2026-05-26 14:42:20 +0200

    add: decided to start from hyper

--- ApplicationLayer/HTTP/1.1/echo_server/Rust/client.rs
+++ ApplicationLayer/HTTP/1.1/echo_server/Rust/client.rs
@@ -4,3 +4,9 @@ use hyper::body::Bytes;
 use hyper_util::rt::TokioIo;
 use tokio::net::TcpStream;
 
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    // This is where we will setup our HTTP client requests.
+
+    Ok(())
+}

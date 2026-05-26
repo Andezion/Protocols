d10e84cb30c86ed1e9edc7669cd0034dd7437abf
Andezion <vdoronchenkov@gmail.com>
2026-05-26 13:37:37 +0200

    add: decided to start from hyper

---
+++ ApplicationLayer/HTTP/1.1/echo_server/Rust/server.rs
@@ -0,0 +1,14 @@
+use std::convert::Infallible;
+use std::net::SocketAddr;
+
+use http_body_util::Full;
+use hyper::body::Bytes;
+use hyper::server::conn::http1;
+use hyper::service::service_fn;
+use hyper::{Request, Response};
+use hyper_util::rt::TokioIo;
+use tokio::net::TcpListener;
+
+async fn handle_request(_: Request<hyper::body::Incoming>) -> Result<Responce<Full<Bytes>>, Infallible> {
+    
+} 

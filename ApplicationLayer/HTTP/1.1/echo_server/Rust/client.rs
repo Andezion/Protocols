64d0c4cdf6d1d09f40ae30aad32eed7519772247
Andezion <vdoronchenkov@gmail.com>
2026-05-26 14:45:20 +0200

    add: decided to start from hyper

--- ApplicationLayer/HTTP/1.1/echo_server/Rust/client.rs
+++ ApplicationLayer/HTTP/1.1/echo_server/Rust/client.rs
@@ -6,7 +6,24 @@ use tokio::net::TcpStream;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    // This is where we will setup our HTTP client requests.
+    let url = "http://httpbin.org/ip".parse::<hyper::Uri>()?;
+
+    let host = url.host().expect("uri has no host");
+    let port = url.port_u16().unwrap_or(80);
+
+    let address = format!("{}:{}", host, port);
+
+    let stream = TcpStream::connect(address).await?;
+
+    let io = TokioIo::new(stream);
+
+    let (mut sender, conn) = hyper::client::conn::http1::handshake(io).await?;
+
+    tokio::task::spawn(async move {
+        if let Err(err) = conn.await {
+            println!("Connection failed: {:?}", err);
+        }
+    });
 
     Ok(())
 }

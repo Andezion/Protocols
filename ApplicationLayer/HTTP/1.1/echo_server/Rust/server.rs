6af3dc53835d50ca075bf7c22a3b9b96765630a0
Andezion <vdoronchenkov@gmail.com>
2026-05-26 13:45:58 +0200

    add: decided to start from hyper

--- ApplicationLayer/HTTP/1.1/echo_server/Rust/server.rs
+++ ApplicationLayer/HTTP/1.1/echo_server/Rust/server.rs
@@ -15,5 +15,22 @@ async fn handle_request(_: Request<hyper::body::Incoming>) -> Result<Response<Fu
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    let listener = TcpListener::bind(addr).await?;
+
+    println!("Listening on http://{}", addr);
+
+    loop {
+        let (stream, _) = listener.accept().await?;
+        
+        let io = TokioIo::new(stream);
+
+        tokio::task::spawn(async move {
+            if let Err(err) = http1::Builder::new()
+                .serve_connection(io, service_fn(hello))
+                .await
+            {
+                eprintln!("Error serving connection: {:?}", err);
+            }
+        });
 }

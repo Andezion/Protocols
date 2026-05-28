72e08d472262daf5c3739eb6925005aadb3f1845
Andezion <vdoronchenkov@gmail.com>
2026-05-28 17:15:01 +0200

    add: started axum stuff

--- ApplicationLayer/HTTP/1.1/echo_server/Rust/axum/server.rs
+++ ApplicationLayer/HTTP/1.1/echo_server/Rust/axum/server.rs
@@ -4,3 +4,17 @@ use axum::{
     Json, Router,
 }
 use serde::{Deserialize, Serialize};
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/users", post(users));
+
+    let listener = tokio::net::TcpListener::bind("0:0:0:0:8080").await.unwrap();
+    println!("Listening on http://{}", listener.local_addr().unwrap());
+
+    axum::serve(listener, app).await.unwrap();
+}

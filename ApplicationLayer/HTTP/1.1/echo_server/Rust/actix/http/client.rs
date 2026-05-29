c4a17c1a3e33e81e0a64b15fe687afb210968e80
Andezion <vdoronchenkov@gmail.com>
2026-05-29 16:57:33 +0200

    add: working on client actix

--- ApplicationLayer/HTTP/1.1/echo_server/Rust/actix/http/client.rs
+++ ApplicationLayer/HTTP/1.1/echo_server/Rust/actix/http/client.rs
@@ -1,29 +1,21 @@
 use std::process::Command;
 
 fn main() -> std::io::Result<()> {
-    let output = Command::new("curl")
-        .arg("-i")
-        .arg("-X")
-        .arg("POST")
-        .arg("http://localhost:8080/users")
-        .arg("-H")
-        .arg("Content-Type: application/json")
-        .arg("-d")
-        .arg(r#"{"name": "angmar"}"#)
-        .output()?;
-
-    println!("Status: {}", output.status);
-    println!("Response: {}", String::from_utf8_lossy(&output.stdout));
-
     let output_empty = Command::new("curl")
     .arg("-i")
     .arg("http://127.0.0.1:8080/")
     .output()?;
 
+    println!("Status: {}", output.status);
+    println!("Response: {}", String::from_utf8_lossy(&output.stdout));
+
     let output_hey = Command::new("curl")
     .arg("-i")
     .arg("http://127.0.0.1:8080/hey")
     .output()?;
+
+    println!("Status: {}", output.status);
+    println!("Response: {}", String::from_utf8_lossy(&output.stdout));
 
     Ok(())
 }

0fd393ca56929818983824ddc6c0b7818ef4ca04
Andezion <vdoronchenkov@gmail.com>
2026-05-29 17:01:15 +0200

    add: working on client actix

--- ApplicationLayer/HTTP/1.1/echo_server/Rust/actix/http/client.rs
+++ ApplicationLayer/HTTP/1.1/echo_server/Rust/actix/http/client.rs
@@ -17,5 +17,19 @@ fn main() -> std::io::Result<()> {
     println!("Status: {}", output_hey.status);
     println!("Response: {}", String::from_utf8_lossy(&output_hey.stdout));
 
+    let output_echo = Command::new("curl")
+    .arg("-i")
+    .arg("-X")
+    .arg("POST")
+    .arg("http://127.0.0.1:8080/echo")
+    .arg("-H")
+    .arg("Content-Type: text/plain")
+    .arg("-d")
+    .arg("zalupa")
+    .output()?;
+
+    println!("Status: {}", output_echo.status);
+    println!("Response: {}", String::from_utf8_lossy(&output_echo.stdout));
+
     Ok(())
 }

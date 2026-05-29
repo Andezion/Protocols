e74293818f097c638ea64a1dfb273b1177b34250
Andezion <vdoronchenkov@gmail.com>
2026-05-29 16:30:37 +0200

    add: started acrix http

--- ApplicationLayer/HTTP/1.1/echo_server/Rust/actix/web/server.rs
+++ ApplicationLayer/HTTP/1.1/echo_server/Rust/actix/web/server.rs
@@ -4,3 +4,8 @@ use actix_web::{get, post, web, App, HttpRequest, HttpServer, Responder};
 async fn func_hello() -> impl Responder {
     HttpResponse::Ok().body("Hello, World!")
 }
+
+#[post("/echo")]
+async fn func_echo(req: String) -> impl Responder {
+    HttpResponse::Ok().body(req)
+}

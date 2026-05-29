31b4ae5459539b1c8290746ce07fc79c3530449c
Andezion <vdoronchenkov@gmail.com>
2026-05-29 17:53:19 +0200

    add: started huge server

--- TransportLayer/TCP/1000_clients/Go/clients/clients.go
+++ TransportLayer/TCP/1000_clients/Go/clients/clients.go
@@ -1,6 +1,14 @@
 package main
 
+import (
+	"bufio"
+	"net"
+)
+
 type Client struct {
-	name string
-	id   int
+	name   string
+	id     int
+	conn   net.Conn
+	reader *bufio.Reader
+	writer *bufio.Writer
 }

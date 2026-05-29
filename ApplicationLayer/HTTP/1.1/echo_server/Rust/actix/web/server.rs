cd19652bd857e9eddec3d1cd2256966d9c0387d7
Andezion <vdoronchenkov@gmail.com>
2026-05-29 16:31:43 +0200

    add: started acrix http

--- ApplicationLayer/HTTP/1.1/echo_server/Rust/actix/web/server.rs
+++ ApplicationLayer/HTTP/1.1/echo_server/Rust/actix/web/server.rs
@@ -14,3 +14,15 @@ async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hello, World!")
 }
 
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}

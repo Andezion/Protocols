445ea250bbc45d3728a33c231d1cfaf448c5b89d
Andezion <vdoronchenkov@gmail.com>
2026-05-29 17:54:12 +0200

    add: added new stuff

--- TransportLayer/TCP/1000_clients/Go/clients/clients.go
+++ TransportLayer/TCP/1000_clients/Go/clients/clients.go
@@ -12,3 +12,29 @@ type Client struct {
 	reader *bufio.Reader
 	writer *bufio.Writer
 }
+
+func NewClient(name string, id int, conn net.Conn) *Client {
+	return &Client{
+		name:   name,
+		id:     id,
+		conn:   conn,
+		reader: bufio.NewReader(conn),
+		writer: bufio.NewWriter(conn),
+	}
+}
+
+func (c *Client) SendMessage(message string) error {
+	_, err := c.writer.WriteString(message + "\n")
+	if err != nil {
+		return err
+	}
+	return c.writer.Flush()
+}
+
+func (c *Client) ReadMessage() (string, error) {
+	return c.reader.ReadString('\n')
+}
+
+func (c *Client) Close() error {
+	return c.conn.Close()
+}

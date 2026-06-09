5f5a4fcbe043906968e02e2246f36117a0331bd5
Andezion <vdoronchenkov@gmail.com>
2026-06-09 11:52:48 +0200

    fixL fixed styling

--- ApplicationLayer/DNS/echo_client_server/Rust/server.rs
+++ ApplicationLayer/DNS/echo_client_server/Rust/server.rs
@@ -1,9 +1,10 @@
 use rust_dns::{DnsServer, DnsRecord};
+
 fn main() -> std::io::Result<()> {
-    // Create a new DNS server instance
     let server = DnsServer::new("127.0.0.1:5300")?;
-    server.add_record(DnsRecord::new("example.com", "192.168.1.1"));
+
+    server.add_record(DnsRecord::new("example.com", "127.0.0.1"));
+    server.add_record(DnsRecord::new("zalupa.com", "127.0.0.1"));
     
-    // Run the server
     server.run()
 }

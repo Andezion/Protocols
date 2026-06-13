6252a49e49840a2496520182a60b0d32d86c634e
Andezion <vdoronchenkov@gmail.com>
2026-06-13 17:29:46 +0200

    add: added basic imports for http stuff

---
+++ ApplicationLayer/HTTP/1.1/echo_server/Zig/server.zig
@@ -0,0 +1,8 @@
+const std = @import("std");
+const log = std.log;
+const net = std.net;
+const os = std.os;
+const http = std.http;
+
+const Request = std.http.Server.Request;
+const Response = std.http.Server.Response;

7c51d525a78c78226945073771548661ba4faed3
Andezion <vdoronchenkov@gmail.com>
2026-06-13 17:39:49 +0200

    add: added basic imports for http stuff

--- ApplicationLayer/HTTP/1.1/echo_server/Zig/server.zig
+++ ApplicationLayer/HTTP/1.1/echo_server/Zig/server.zig
@@ -6,3 +6,11 @@ const http = std.http;
 
 const Request = std.http.Server.Request;
 const Response = std.http.Server.Response;
+
+pub fn main(init: std.process.Init) !void {
+    const io = init.io;
+
+    const addr = try net.IpAddress.parse("127.0.0.1", 8080);
+    var server = try addr.listen(io, .{ .reuse_address = true });
+    defer server.close(io);
+}

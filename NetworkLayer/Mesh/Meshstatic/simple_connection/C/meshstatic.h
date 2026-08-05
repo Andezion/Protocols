edfdf439187bed4ca4581694dd1abc56b6dbb1d0
Andezion <vdoronchenkov@gmail.com>
2026-08-05 09:16:26 +0200

    add: working on mesh

--- NetworkLayer/Mesh/Meshstatic/simple_connection/C/meshstatic.h
+++ NetworkLayer/Mesh/Meshstatic/simple_connection/C/meshstatic.h
@@ -10,4 +10,7 @@ extern void sx1262_send_packet(uint8_t *buffer, size_t len);
 // Собственно сама отправка сообщения в Meshtastic (текстовый чат)
 void send_mesh_text_message(const char* text);
 
-void send_data();
+void send_data(); // тут создаём Data и заполняем его поля 
+void send_mesh_packet(); // тут создаем MeshPacket, шифруем и отправляем его по радиоканалу
+void receive_mesh_packet(); // тут принимаем пакет, расшифровываем наш пакет
+void handle_data(); // тут обрабатываем полученные данные

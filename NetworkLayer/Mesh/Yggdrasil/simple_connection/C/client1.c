b1526cf883984749ebadae6c283211d3b34397b1
Andezion <vdoronchenkov@gmail.com>
2026-08-15 00:14:44 +0200

    add: added bacis ygg staff

--- NetworkLayer/Mesh/Yggdrasil/simple_connection/C/client1.c
+++ NetworkLayer/Mesh/Yggdrasil/simple_connection/C/client1.c
@@ -7,8 +7,7 @@
 
 #include "yggdrasil.h"
 
-// client1 - принимающий узел ("сначала слушаем, потом кто-то к нам приходит",
-// как узел Yggdrasil, ожидающий входящий пиринг).
+// client1 - принимающий узел, сначала слушаем, потом кто-то к нам приходит
 int main(void) {
     struct Yggdrasil node;
     if (ygg_init(&node) != 0) {
@@ -52,18 +51,25 @@ int main(void) {
         perror("accept"); 
         return 1; 
     }
-    
+
     printf("[client1] peer connected, exchanging metadata...\n");
 
-    if (ygg_send_metadata(conn, &node) != 0) { fprintf(stderr, "send metadata failed\n"); return 1; }
+    if (ygg_send_metadata(conn, &node) != 0) { 
+        fprintf(stderr, "send metadata failed\n"); 
+        return 1; 
+    }
 
     unsigned char peer_ed_pk[crypto_sign_PUBLICKEYBYTES];
-    if (ygg_recv_metadata(conn, peer_ed_pk) != 0) { fprintf(stderr, "recv metadata failed\n"); return 1; }
+    if (ygg_recv_metadata(conn, peer_ed_pk) != 0) { 
+        fprintf(stderr, "recv metadata failed\n"); 
+        return 1; 
+    }
 
     unsigned char peer_addr[YGG_ADDR_SIZE];
     ygg_addr_for_key(peer_ed_pk, peer_addr);
     char peeraddrstr[46];
     ygg_addr_to_string(peer_addr, peeraddrstr);
+
     printf("[client1] peer identity confirmed, peer Yggdrasil address: %s\n", peeraddrstr);
 
     unsigned char peer_box_pk[crypto_box_PUBLICKEYBYTES];
@@ -81,11 +87,13 @@ int main(void) {
 
     unsigned char reply[256];
     size_t replylen = 0;
+
     if (ygg_recv_encrypted(conn, &node, peer_box_pk, reply, sizeof(reply) - 1, &replylen) != 0) {
         fprintf(stderr, "recv encrypted failed\n");
         return 1;
     }
     reply[replylen] = '\0';
+    
     printf("[client1] received decrypted: %s\n", reply);
 
     close(conn);
